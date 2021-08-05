7de948f92b6990640806839330f5b1d401f014f5
Nate Moore <nate@skypack.dev>
2021-08-05 16:10:00 -0500

    update deno package

--- lib/compiler/deno/wasm_exec.ts
+++ lib/compiler/deno/wasm_exec.ts
@@ -6,88 +6,40 @@
 // This file has been modified for use by the TinyGo compiler.
 // This file has been further modified for use by Astro.
 
-
-const enosys = () => {
-	const err = new Error("not implemented");
-	err.code = "ENOSYS";
-	return err;
-};
-
-let outputBuf = "";
-const fs = {
-	constants: { O_WRONLY: -1, O_RDWR: -1, O_CREAT: -1, O_TRUNC: -1, O_APPEND: -1, O_EXCL: -1 }, // unused
-	writeSync(fd, buf) {
-		outputBuf += decoder.decode(buf);
-		const nl = outputBuf.lastIndexOf("\n");
-		if (nl != -1) {
-			console.log(outputBuf.substr(0, nl));
-			outputBuf = outputBuf.substr(nl + 1);
-		}
-		return buf.length;
-	},
-	write(fd, buf, offset, length, position, callback) {
-		if (offset !== 0 || length !== buf.length || position !== null) {
-			callback(enosys());
-			return;
-		}
-		const n = this.writeSync(fd, buf);
-		callback(null, n);
-	},
-	chmod(path, mode, callback) { callback(enosys()); },
-	chown(path, uid, gid, callback) { callback(enosys()); },
-	close(fd, callback) { callback(enosys()); },
-	fchmod(fd, mode, callback) { callback(enosys()); },
-	fchown(fd, uid, gid, callback) { callback(enosys()); },
-	fstat(fd, callback) { callback(enosys()); },
-	fsync(fd, callback) { callback(null); },
-	ftruncate(fd, length, callback) { callback(enosys()); },
-	lchown(path, uid, gid, callback) { callback(enosys()); },
-	link(path, link, callback) { callback(enosys()); },
-	lstat(path, callback) { callback(enosys()); },
-	mkdir(path, perm, callback) { callback(enosys()); },
-	open(path, flags, mode, callback) { callback(enosys()); },
-	read(fd, buffer, offset, length, position, callback) { callback(enosys()); },
-	readdir(path, callback) { callback(enosys()); },
-	readlink(path, callback) { callback(enosys()); },
-	rename(from, to, callback) { callback(enosys()); },
-	rmdir(path, callback) { callback(enosys()); },
-	stat(path, callback) { callback(enosys()); },
-	symlink(path, link, callback) { callback(enosys()); },
-	truncate(path, length, callback) { callback(enosys()); },
-	unlink(path, callback) { callback(enosys()); },
-	utimes(path, atime, mtime, callback) { callback(enosys()); },
-};
-const process = {
-	getuid() { return -1; },
-	getgid() { return -1; },
-	geteuid() { return -1; },
-	getegid() { return -1; },
-	getgroups() { throw enosys(); },
-	pid: -1,
-	ppid: -1,
-	umask() { throw enosys(); },
-	cwd() { throw enosys(); },
-	chdir() { throw enosys(); },
-}
+import fs from "https://deno.land/std@0.103.0/node/fs.ts";
+import { randomFillSync } from "https://deno.land/std@0.103.0/node/crypto.ts";
+import { TextEncoder, TextDecoder } from "https://deno.land/std@0.103.0/node/util.ts";
 
 Object.defineProperties(globalThis, {
   fs: {
     value: fs,
     enumerable: true,
   },
-  process: {
-	value: process,
-	enumerable: true
-  }
+  crypto: {
+    value: {
+      getRandomValues(b) {
+        randomFillSync(b);
+      },
+    },
+    enumerable: true,
+  },
+  performance: {
+    value: {
+      now() {
+        const [sec, nsec] = process.hrtime();
+        return sec * 1000 + nsec / 1000000;
+      },
+    },
+    enumerable: true,
+  },
 });
 
 const encoder = new TextEncoder("utf-8");
 const decoder = new TextDecoder("utf-8");
-var logLine = [];
+var logLine: any[] = [];
 
 export default class Go {
   importObject: Record<string, any>;
-
   constructor() {
     this._callbackTimeouts = new Map();
     this._nextCallbackTimeoutID = 1;
