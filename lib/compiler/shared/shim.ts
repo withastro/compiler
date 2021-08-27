382ec2e5b152ee6d7b088e9350e889cc1971a1e7
Nate Moore <nate@skypack.dev>
2021-08-27 13:32:34 -0500

    chore: new WASM build

--- lib/compiler/shared/shim.ts
+++ lib/compiler/shared/shim.ts
@@ -1,79 +1,42 @@
-// @ts-nocheck
-const voidTags = new Set(["area", "base", "br", "col", "command", "embed", "hr", "img", "input", "keygen", "link", "meta", "param", "source", "track", "wbr"]);
-function* _h(tag, attrs, children) {
-  if (tag.toLowerCase() === "!doctype") {
-    yield `<${tag} `;
-    if (attrs) {
-      yield Object.keys(attrs).join(" ");
-    }
-    yield ">";
-    return;
-  }
-  yield `<${tag}`;
-  if (attrs) {
-    for (let [key, value] of Object.entries(attrs)) {
-      if (value === "")
-        yield ` ${key}=""`;
-      else if (value == null || value === false)
-        yield "";
-      else if (value === true)
-        yield ` ${key}`;
-      else
-        yield ` ${key}="${value}"`;
-    }
-  }
-  yield ">";
-  if (voidTags.has(tag)) {
-    return;
-  }
-  for (let child of children) {
-    if (typeof child === "function") {
-      yield child();
-    } else if (typeof child === "string") {
-      yield child;
-    } else if (!child && child !== 0) {
-    } else {
-      yield child;
-    }
-  }
-  yield `</${tag}>`;
+export const render = async (htmlParts: string[], expressions: TemplateStringsArray) => {
+  console.log("render", { htmlParts, expressions });
+  return 'Not implemented'
 }
-async function h(tag, attrs, ...pChildren) {
-  const children = await Promise.all(pChildren.flat(Infinity));
-  if (typeof tag === "function") {
-    return tag(attrs, ...children);
-  }
-  return Array.from(_h(tag, attrs, children)).join("");
-}
-function Fragment(_, ...children) {
-  console.log(children);
-  return children.join("");
-}
-function __astro_slot_content({name}, ...children) {
-  return {$slot: name, children};
-}
-const __astro_slot = ({name = "default"}, _children, ...fallback) => {
-  if (name === "default" && typeof _children === "string") {
-    return _children ? _children : fallback;
-  }
-  if (!_children.$slots) {
-    throw new Error(`__astro_slot encountered an unexpected child:
-${JSON.stringify(_children)}`);
-  }
-  const children = _children.$slots[name];
-  return children ? children : fallback;
-};
 
-// @ts-ignore
-globalThis.__astro_slot = __astro_slot;
-globalThis.__astro_slot_content = __astro_slot_content;
-globalThis.Fragment = Fragment;
-globalThis.h = h;
-globalThis.Astro = {};
-globalThis.__astro_component = async (context, ...children) => {
-  const result = await context.Component();
-  console.log('__astro_component', context.Component.toString());
-  // const { Component } = context;
-  // const output = renderToString(html`<${Component}>...${children}</${Component}>`);
-  return `<astro-root>${result}</astro-root>`;
+export interface CompnentCallback {
+  (result: any, props: any, slots: any): ReturnType<typeof render>
+}
+
+export const createComponent = async (cb: CompnentCallback) => Object.assign(cb, { isAstroComponent: true });
+
+export const renderComponent = (Component: any, props: any, children: any) => {
+  console.log("renderComponent", { Component, props, children });
+  return '';
+}
+
+export const addAttribute = (value: any, key: string) => {
+  return ` ${key}="${value}"`;
+}
+
+export const spreadAttributes = (values: Record<any, any>) => {
+  let output = '';
+  for (const [key, value] of Object.entries(values)) {
+    output += addAttribute(value, key);
+  }
+  return output;
+}
+
+export const defineStyleVars = (astroId: string, vars: Record<any, any>) => {
+  let output = '\n';
+  for (const [key, value] of Object.entries(vars)) {
+    output += `--${key}: ${value};\n`;
+  }
+  return `.${astroId} {${output}}`
+}
+export const defineScriptVars = (vars: Record<any, any>) => {
+  let output = '';
+  for (const [key, value] of Object.entries(vars)) {
+    output += `let ${key} = ${value};\n`;
+  }
+  return output;
 }
