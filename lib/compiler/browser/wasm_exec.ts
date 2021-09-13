fe7ab330e12655690548c5783b47fd887b2ddb83
Okiki <okikio.dev@gmail.com>
2021-09-13 18:23:21 -0400

    use proper polyfills for process and memfs

--- lib/compiler/browser/wasm_exec.ts
+++ lib/compiler/browser/wasm_exec.ts
@@ -6,6 +6,8 @@
 // This file has been modified for use by the TinyGo compiler.
 // This file has been further modified for use by Astro.
 
+import { fs, vol } from "memfs";
+vol.fromJSON({}, "/");
 
 const enosys = () => {
 	const err = new Error("not implemented");
@@ -14,49 +16,6 @@ const enosys = () => {
 };
 
 let outputBuf = "";
-const fs = {
-	constants: { O_WRONLY: -1, O_RDWR: -1, O_CREAT: -1, O_TRUNC: -1, O_APPEND: -1, O_EXCL: -1 }, // unused
-	writeSync(fd, buf) {
-		outputBuf += decoder.decode(buf);
-		const nl = outputBuf.lastIndexOf("\n");
-		if (nl != -1) {
-			console.log(outputBuf.substr(0, nl));
-			outputBuf = outputBuf.substr(nl + 1);
-		}
-		return buf.length;
-	},
-	write(fd, buf, offset, length, position, callback) {
-		if (offset !== 0 || length !== buf.length || position !== null) {
-			callback(enosys());
-			return;
-		}
-		const n = this.writeSync(fd, buf);
-		callback(null, n);
-	},
-	chmod(path, mode, callback) { callback(enosys()); },
-	chown(path, uid, gid, callback) { callback(enosys()); },
-	close(fd, callback) { callback(enosys()); },
-	fchmod(fd, mode, callback) { callback(enosys()); },
-	fchown(fd, uid, gid, callback) { callback(enosys()); },
-	fstat(fd, callback) { callback(enosys()); },
-	fsync(fd, callback) { callback(null); },
-	ftruncate(fd, length, callback) { callback(enosys()); },
-	lchown(path, uid, gid, callback) { callback(enosys()); },
-	link(path, link, callback) { callback(enosys()); },
-	lstat(path, callback) { callback(enosys()); },
-	mkdir(path, perm, callback) { callback(enosys()); },
-	open(path, flags, mode, callback) { callback(enosys()); },
-	read(fd, buffer, offset, length, position, callback) { callback(enosys()); },
-	readdir(path, callback) { callback(enosys()); },
-	readlink(path, callback) { callback(enosys()); },
-	rename(from, to, callback) { callback(enosys()); },
-	rmdir(path, callback) { callback(enosys()); },
-	stat(path, callback) { callback(enosys()); },
-	symlink(path, link, callback) { callback(enosys()); },
-	truncate(path, length, callback) { callback(enosys()); },
-	unlink(path, callback) { callback(enosys()); },
-	utimes(path, atime, mtime, callback) { callback(enosys()); },
-};
 const process = {
 	getuid() { return -1; },
 	getgid() { return -1; },
@@ -69,6 +28,260 @@ const process = {
 	cwd() { throw enosys(); },
 	chdir() { throw enosys(); },
 }
+
+// @ts-nocheck
+// Shim for using process in browser
+// Based off https://github.com/defunctzombie/node-process/blob/master/browser.js
+// Based off https://github.com/hardfist/neo-tools/blob/main/packages/bundler/src/shim/process.js
+
+function defaultSetTimout() {
+    throw new Error('setTimeout has not been defined');
+}
+function defaultClearTimeout() {
+    throw new Error('clearTimeout has not been defined');
+}
+var cachedSetTimeout = defaultSetTimout;
+var cachedClearTimeout = defaultClearTimeout;
+if (typeof globalThis.setTimeout === 'function') {
+    cachedSetTimeout = setTimeout;
+}
+if (typeof globalThis.clearTimeout === 'function') {
+    cachedClearTimeout = clearTimeout;
+}
+
+function runTimeout(fun) {
+    if (cachedSetTimeout === setTimeout) {
+        //normal enviroments in sane situations
+        return setTimeout(fun, 0);
+    }
+    // if setTimeout wasn't available but was latter defined
+    if ((cachedSetTimeout === defaultSetTimout || !cachedSetTimeout) && setTimeout) {
+        cachedSetTimeout = setTimeout;
+        return setTimeout(fun, 0);
+    }
+    try {
+        // when when somebody has screwed with setTimeout but no I.E. maddness
+        return cachedSetTimeout(fun, 0);
+    } catch (e) {
+        try {
+            // When we are in I.E. but the script has been evaled so I.E. doesn't trust the global object when called normally
+            return cachedSetTimeout.call(null, fun, 0);
+        } catch (e) {
+            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error
+            return cachedSetTimeout.call(this, fun, 0);
+        }
+    }
+}
+function runClearTimeout(marker) {
+    if (cachedClearTimeout === clearTimeout) {
+        //normal enviroments in sane situations
+        return clearTimeout(marker);
+    }
+    // if clearTimeout wasn't available but was latter defined
+    if ((cachedClearTimeout === defaultClearTimeout || !cachedClearTimeout) && clearTimeout) {
+        cachedClearTimeout = clearTimeout;
+        return clearTimeout(marker);
+    }
+    try {
+        // when when somebody has screwed with setTimeout but no I.E. maddness
+        return cachedClearTimeout(marker);
+    } catch (e) {
+        try {
+            // When we are in I.E. but the script has been evaled so I.E. doesn't  trust the global object when called normally
+            return cachedClearTimeout.call(null, marker);
+        } catch (e) {
+            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error.
+            // Some versions of I.E. have different rules for clearTimeout vs setTimeout
+            return cachedClearTimeout.call(this, marker);
+        }
+    }
+}
+var queue = [];
+var draining = false;
+var currentQueue;
+var queueIndex = -1;
+
+function cleanUpNextTick() {
+    if (!draining || !currentQueue) {
+        return;
+    }
+    draining = false;
+    if (currentQueue.length) {
+        queue = currentQueue.concat(queue);
+    } else {
+        queueIndex = -1;
+    }
+    if (queue.length) {
+        drainQueue();
+    }
+}
+
+function drainQueue() {
+    if (draining) {
+        return;
+    }
+    var timeout = runTimeout(cleanUpNextTick);
+    draining = true;
+
+    var len = queue.length;
+    while (len) {
+        currentQueue = queue;
+        queue = [];
+        while (++queueIndex < len) {
+            if (currentQueue) {
+                currentQueue[queueIndex].run();
+            }
+        }
+        queueIndex = -1;
+        len = queue.length;
+    }
+    currentQueue = null;
+    draining = false;
+    runClearTimeout(timeout);
+}
+function nextTick(fun) {
+    var args = new Array(arguments.length - 1);
+    if (arguments.length > 1) {
+        for (var i = 1; i < arguments.length; i++) {
+            args[i - 1] = arguments[i];
+        }
+    }
+    queue.push(new Item(fun, args));
+    if (queue.length === 1 && !draining) {
+        runTimeout(drainQueue);
+    }
+}
+// v8 likes predictible objects
+function Item(fun, array) {
+    this.fun = fun;
+    this.array = array;
+}
+Item.prototype.run = function () {
+    this.fun.apply(null, this.array);
+};
+var title = 'browser';
+var platform = 'browser';
+var browser = true;
+var env = {};
+var argv = [];
+var version = ''; // empty string to avoid regexp issues
+var versions = {};
+var release = {};
+var config = {};
+
+function noop() { }
+
+var on = noop;
+var addListener = noop;
+var once = noop;
+var off = noop;
+var removeListener = noop;
+var removeAllListeners = noop;
+var emit = noop;
+
+function binding(name) {
+    throw new Error('process.binding is not supported');
+}
+
+function cwd() {
+    return '/';
+}
+function chdir(dir) {
+    throw new Error('process.chdir is not supported');
+}
+function umask() {
+    return 0;
+}
+
+// from https://github.com/kumavis/browser-process-hrtime/blob/master/index.js
+var performance = globalThis.performance || {};
+var performanceNow =
+    performance.now ||
+    performance.mozNow ||
+    performance.msNow ||
+    performance.oNow ||
+    performance.webkitNow ||
+    function () {
+        return new Date().getTime();
+    };
+
+// generate timestamp or delta
+// see http://nodejs.org/api/process.html#process_process_hrtime
+function hrtime(previousTimestamp) {
+    var clocktime = performanceNow.call(performance) * 1e-3;
+    var seconds = Math.floor(clocktime);
+    var nanoseconds = Math.floor((clocktime % 1) * 1e9);
+    if (previousTimestamp) {
+        seconds = seconds - previousTimestamp[0];
+        nanoseconds = nanoseconds - previousTimestamp[1];
+        if (nanoseconds < 0) {
+            seconds--;
+            nanoseconds += 1e9;
+        }
+    }
+    return [seconds, nanoseconds];
+}
+
+var startTime = new Date();
+function uptime() {
+    var currentTime = new Date();
+    var dif = currentTime - startTime;
+    return dif / 1000;
+}
+
+var browser$1 = {
+    nextTick: nextTick,
+    title: title,
+    browser: browser,
+    env: env,
+    argv: argv,
+    version: version,
+    versions: versions,
+    on: on,
+    addListener: addListener,
+    once: once,
+    off: off,
+    removeListener: removeListener,
+    removeAllListeners: removeAllListeners,
+    emit: emit,
+    binding: binding,
+    cwd: cwd,
+    chdir: chdir,
+    umask: umask,
+    hrtime: hrtime,
+    platform: platform,
+    release: release,
+    config: config,
+    uptime: uptime,
+};
+
+export default browser$1;
+delete versions['node']; // hack
+Object.assign(process, {
+    addListener,
+    argv,
+    binding,
+    browser,
+    chdir,
+    config,
+    cwd,
+    emit,
+    env,
+    hrtime,
+    nextTick,
+    off,
+    on,
+    once,
+    platform,
+    release,
+    removeAllListeners,
+    removeListener,
+    title,
+    umask,
+    uptime,
+    version,
+    versions,
+}, browser$1);
 
 Object.defineProperties(globalThis, {
   fs: {
