9372c103286b435e1ddd8396a56248fd0aaf85d3
Nate Moore <natemoo-re@users.noreply.github.com>
2021-10-26 14:32:07 -0700

    scope `<html>` elements to fix a `define:vars` bug (#109)
    * fix: scope HTML element to fix `define:vars` bug
    
    * refactor: cleanup globalElement util
    
    * chore: add changeset

---
+++ .changeset/rotten-pigs-agree.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/compiler': patch
+---
+
+Support `define:vars` with root `html` element on pages
--- internal/transform/scope-css.go
+++ internal/transform/scope-css.go
@@ -223,24 +223,8 @@ func scopeRule(id string, opts TransformOptions) string {
 
 // Get list of elements that should be scoped
 func globalElement(id string) bool {
-	switch id {
-	case "base",
-		"body",
-		"font",
-		"frame",
-		"frameset",
-		"head",
-		"html",
-		"link",
-		"meta",
-		"noframes",
-		"noscript",
-		"script",
-		"style",
-		"title",
-		":root":
+	if NeverScopedElements[id] || NeverScopedSelectors[id] {
 		return true
-	default:
-		return false
 	}
+	return false
 }
