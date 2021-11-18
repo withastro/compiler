c27aec9cccae501731b5f0379161c9de86c6a251
Nate Moore <nate@skypack.dev>
2021-11-18 14:03:20 -0600

    fix: handle defers in LIFO order

---
+++ .changeset/sour-bees-bathe.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/compiler': patch
+---
+
+Fix "call to released function" issue
--- internal_wasm/utils/utils.go
+++ internal_wasm/utils/utils.go
@@ -9,24 +9,26 @@ import (
 // See https://stackoverflow.com/questions/68426700/how-to-wait-a-js-async-function-from-golang-wasm
 func Await(awaitable js.Value) ([]js.Value, []js.Value) {
 	then := make(chan []js.Value)
-	defer close(then)
 	thenFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		then <- args
 		return nil
 	})
+	// defers are called LIFO!
+	// This will `close` before `Release()`
+	defer thenFunc.Release()
+	defer close(then)
 
 	catch := make(chan []js.Value)
-	defer close(catch)
 	catchFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		catch <- args
 		return nil
 	})
+	// defers are called LIFO!
+	// This will `close` before `Release()`
+	defer catchFunc.Release()
+	defer close(catch)
 
 	awaitable.Call("then", thenFunc).Call("catch", catchFunc)
-
-	// only release after awaitable.Call
-	defer thenFunc.Release()
-	defer catchFunc.Release()
 
 	select {
 	case result := <-then:
