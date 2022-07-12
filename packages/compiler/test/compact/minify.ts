83395504d7d72b4857d628bc6854ae32272bd102
Nate Moore <nate@astro.build>
2022-07-12 10:27:44 -0500

    test: fix minification tests

--- packages/compiler/test/compact/minify.ts
+++ packages/compiler/test/compact/minify.ts
@@ -4,12 +4,12 @@ import { transform } from '@astrojs/compiler';
 
 async function minify(input: string) {
   const code = (await transform(input, { compact: true })).code;
-  return code;
+  return code.replace('${$$maybeRenderHead($$result)}', '');
 }
 
 test('basic', async () => {
-  assert.match(await minify(`    <div>Hello {value}</div>      `), '$$render`<div>Hello ${value}</div> `');
-  assert.match(await minify(`    <div> Hello {value} </div>      `), '$$render`<div> Hello ${value} </div> `');
+  assert.match(await minify(`    <div>Hello {value}!</div>      `), '$$render`<div>Hello ${value}!</div>`');
+  assert.match(await minify(`    <div> Hello {value}! </div>      `), '$$render`<div> Hello ${value}! </div>`');
 });
 
 test('preservation', async () => {
@@ -47,9 +47,9 @@ test('space normalization around text', async () => {
   assert.match(await minify('<p>foo<wbr baz moo="">bar</p>'), '<p>foo<wbr baz moo="">bar</p>');
   assert.match(await minify('<p>foo <wbr baz moo="">bar</p>'), '<p>foo <wbr baz moo="">bar</p>');
   assert.match(await minify('<p>foo<wbr baz moo=""> bar</p>'), '<p>foo<wbr baz moo=""> bar</p>');
-  assert.match(await minify('<p>  <a href="#">  <code>foo</code></a> bar</p>'), '<p> <a href="#"> <code>foo</code></a> bar</p>');
+  assert.match(await minify('<p>  <a href="#">  <code>foo</code></a> bar</p>'), '<p><a href="#"><code>foo</code></a> bar</p>');
   assert.match(await minify('<p><a href="#"><code>foo  </code></a> bar</p>'), '<p><a href="#"><code>foo </code></a> bar</p>');
-  assert.match(await minify('<p>  <a href="#">  <code>   foo</code></a> bar   </p>'), '<p> <a href="#"> <code> foo</code></a> bar </p>');
+  assert.match(await minify('<p>  <a href="#">  <code>   foo</code></a> bar   </p>'), '<p><a href="#"><code> foo</code></a> bar </p>');
   assert.match(await minify('<div> Empty <!-- or --> not </div>'), '<div> Empty <!-- or --> not </div>');
   assert.match(await minify('<div> a <input><!-- b --> c </div>'), '<div> a <input><!-- b --> c </div>');
   await Promise.all(
