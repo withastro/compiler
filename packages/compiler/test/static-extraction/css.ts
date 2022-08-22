3b9f0d2184f3650b2b3a9329909054cafd2d7f9f
Bjorn Lu <bjornlu.dev@gmail.com>
2022-08-22 21:52:43 +0800

    Remove css print escape for experimentalStaticExtraction (#503)

--- packages/compiler/test/static-extraction/css.ts
+++ packages/compiler/test/static-extraction/css.ts
@@ -7,6 +7,8 @@ const FIXTURE = `
 ---
 <style>
     .thing { color: green; }
+    .url-space { background: url('/white space.png'); }
+    .escape:not(#\\#) { color: red; }
 </style>
 `;
 
@@ -19,6 +21,14 @@ test.before(async () => {
 
 test('extracts styles', () => {
   assert.equal(result.css.length, 1, `Incorrect CSS returned. Expected a length of 1 and got ${result.css.length}`);
+});
+
+test('escape url with space', () => {
+  assert.match(result.css[0], 'background:url(/white\\ space.png)');
+});
+
+test('escape css syntax', () => {
+  assert.match(result.css[0], ':not(#\\#)');
 });
 
 test.run();
