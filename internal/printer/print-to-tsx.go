d34318650fd9498c688cdfd2d4a30f00bb60e257
Nate Moore <nate@astro.build>
2022-10-06 10:30:38 -0500

    chore(tsx): update `Astro` type to use generic

--- internal/printer/print-to-tsx.go
+++ internal/printer/print-to-tsx.go
@@ -66,7 +66,7 @@ func renderTsx(p *printer, n *Node) {
 	if n.Type == DocumentNode {
 		props := js_scanner.GetPropsType([]byte(p.sourcetext))
 		if props.Ident != "Record<string, any>" {
-			p.printf(`declare const Astro: { props: %s } & Omit<import('astro').AstroGlobal, 'props'>`, props.Ident)
+			p.printf(`declare const Astro: import('astro').AstroGlobal<%s>`, props.Ident)
 		}
 		hasChildren := false
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
