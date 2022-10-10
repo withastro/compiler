withastro/compiler

commit 884efc6e9f294ff28e3471db5440f3a0f4573806

fix(#536): multi-line export hoisting (#546)
Co-authored-by: Nate Moore <nate@astro.build>

internal/js_scanner/js_scanner.go
@@ -86,6 +86,10 @@ outer:
 					if isKeyword(nextValue) && next != js.FromToken {
 						continue
 					}
+					// ensure type declarations are defined or exported from package
+					if flags["type"] && !(flags["="] || flags["from"]) {
+						continue
+					}
 					if !foundIdent {
 						foundIdent = true
 					}
@@ -93,7 +97,7 @@ outer:
 						flags["&"] = false
 					}
 				} else if next == js.LineTerminatorToken || next == js.SemicolonToken || (next == js.ErrorToken && l.Err() == io.EOF) {
-					if (flags["function"] || flags["=>"]) && !flags["{"] {
+					if (flags["function"] || flags["=>"] || flags["interface"]) && !flags["{"] {
 						continue
 					}
 					if flags["&"] {

internal/js_scanner/js_scanner_test.go
@@ -384,6 +384,28 @@ export B from "./_types"
 export type C from "./_types"`,
 		},
 		{
+			name: "multi-line export",
+			source: `export interface Props
+{
+	foo: 'bar';
+}`,
+			want: `export interface Props
+{
+	foo: 'bar';
+}`,
+		},
+		{
+			name: "multi-line type export",
+			source: `export type Props =
+{
+	foo: 'bar';
+}`,
+			want: `export type Props =
+{
+	foo: 'bar';
+}`,
+		},
+		{
 			name: "Picture",
 			source: `// @ts-ignore
 import loader from 'virtual:image-loader';

commit 8c89f70faa6f4a38da53e58724cd357c0705cbff

fixed regression caused by #546

internal/js_scanner/js_scanner.go
@@ -83,21 +83,17 @@ outer:
 					if isKeyword(nextValue) && next != js.FromToken {
 						continue
 					}
-					// ensure type declarations are defined or exported from package
-					if flags["type"] && !(flags["="] || flags["from"]) {
+					if string(nextValue) == "type" {
 						continue
 					}
 					if !foundIdent {
 						foundIdent = true
 					}
-					if flags["&"] {
-						flags["&"] = false
-					}
 				} else if next == js.LineTerminatorToken || next == js.SemicolonToken || (next == js.ErrorToken && l.Err() == io.EOF) {
 					if (flags["function"] || flags["=>"] || flags["interface"]) && !flags["{"] {
 						continue
 					}
-					if flags["&"] {
+					if flags["&"] || flags["="] {
 						continue
 					}
 					foundSemicolonOrLineTerminator = true
@@ -111,6 +107,21 @@ outer:
 						pairs['(']--
 					} else if nextValue[0] == ']' {
 						pairs['[']--
+					}
+				} else {
+					// Sometimes, exports are written in multiple lines, like
+					//
+					// export const foo =
+					//   [...]
+					// export type Props = ThisProps &
+					// 	 SomeWeirdType<{ thatsSuperLong: SoItEndsUpFormattedLikeThis }>
+					//
+					// So, we omit the semicolon check if the line ends up with one of these
+					if flags["&"] && nextValue[0] != '&' {
+						flags["&"] = false
+					}
+					if flags["="] && nextValue[0] != '=' {
+						flags["="] = false
 					}
 				}
 

internal/js_scanner/js_scanner_test.go
@@ -406,6 +406,27 @@ export type C from "./_types"`,
 }`,
 		},
 		{
+			name: "multi-line type export with multiple exports",
+			source: `export type Theme = 'light' | 'dark';
+
+export type Props =
+{
+	theme: Theme;
+};
+
+export interface Foo {
+	bar: string;
+}`,
+			want: `export type Theme = 'light' | 'dark';
+export type Props =
+{
+	theme: Theme;
+}
+export interface Foo {
+	bar: string;
+}`,
+		},
+		{
 			name: "Picture",
 			source: `// @ts-ignore
 import loader from 'virtual:image-loader';