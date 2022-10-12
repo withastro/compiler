3699955437b213f56d372fb45d2e9adb7346b651
Princesseuh <princssdev@gmail.com>
2022-10-12 12:23:23 -0300

    Fix parse crashing when a component with a client directive was not found

---
+++ packages/compiler/test/parse/client-component-unfound.ts
@@ -0,0 +1,24 @@
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import { parse } from '@astrojs/compiler';
+
+const FIXTURE = `{
+	headers && (
+		<nav class="mobile-toc">
+			<TableOfContents
+				client:media="(max-width: 72em)"
+				headers={headers}
+				labels={{ onThisPage: t('rightSidebar.onThisPage'), overview: t('rightSidebar.overview') }}
+				isMobile={true}
+			/>
+		</nav>
+	)
+}
+`;
+
+test('unfound client component', async () => {
+  const result = await parse(FIXTURE);
+  assert.ok(result.ast, 'Expected an AST to be generated');
+});
+
+test.run();
