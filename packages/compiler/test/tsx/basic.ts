d7f328865dd0f349dc12c6b83d3b86462886007b
Nate Moore <natemoo-re@users.noreply.github.com>
2022-10-21 14:24:30 -0500

    TSX: improve self-closing tag handling (#581)
    * fix(tsx): improve self-closing tag behavior and mappings
    
    * chore: add changeset
    
    Co-authored-by: Nate Moore <nate@astro.build>

--- packages/compiler/test/tsx/basic.ts
+++ packages/compiler/test/tsx/basic.ts
@@ -165,7 +165,7 @@ export default function __AstroComponent_(_props: Record<string, any>): any {}`;
 test('spread object', async () => {
   const input = `<DocSearch {...{ lang, labels: { modal, placeholder } }} client:only="preact" />`;
   const output = `<Fragment>
-<DocSearch {...{ lang, labels: { modal, placeholder } }} client:only="preact" ></DocSearch>
+<DocSearch {...{ lang, labels: { modal, placeholder } }} client:only="preact" />
 </Fragment>
 export default function __AstroComponent_(_props: Record<string, any>): any {}`;
   const { code } = await convertToTSX(input, { sourcemap: 'external' });
