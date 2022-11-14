6ff1d802a31f5652a02ef070beefac0e0d6b918c
Nate Moore <natemoo-re@users.noreply.github.com>
2022-11-14 10:15:54 -0600

    fix regression for inLiteralIM handling (#627)
    * fix: improve literalIM handling
    
    * chore: remove console
    
    Co-authored-by: Nate Moore <nate@astro.build>

---
+++ packages/compiler/test/basic/body-after-head-component.ts
@@ -0,0 +1,29 @@
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import { transform } from '@astrojs/compiler';
+
+const FIXTURE = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <TestHead />
+    <title>document</title>
+  </head>
+  <body>
+    <main>
+       <h1>Welcome to <span class="text-gradient">Astro</span></h1>
+    </main>
+  </body>
+</html>
+`;
+
+let result;
+test.before(async () => {
+  result = await transform(FIXTURE);
+});
+
+test('has body in output', () => {
+  assert.match(result.code, '<body>', 'Expected output to contain body element!');
+});
+
+test.run();
