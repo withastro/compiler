69fb07addf943086603f2ed887ca4103bb69c8ac
Nate Moore <nate@astro.build>
2022-12-02 15:28:29 -0600

    test: better assertions

--- packages/compiler/test/bad-styles/unclosed-style.ts
+++ packages/compiler/test/bad-styles/unclosed-style.ts
@@ -4,12 +4,17 @@ import { parse } from '@astrojs/compiler';
 
 test('can compile unfinished style', async () => {
   let error = 0;
+  let result;
   try {
-    await parse(`<style>`);
+    result = await parse(`<style>`);
   } catch (e) {
     error = 1;
   }
+
+  const style = result.ast.children[0];
   assert.equal(error, 0, 'Expected to compile with unfinished style.');
+  assert.ok(result.ast, 'Expected to compile with unfinished style.');
+  assert.equal(style.name, 'style', 'Expected to compile with unfinished style.');
 });
 
 test.run();
