329656512092427d68c5929902d667fde9d50621
Caleb Jasik <calebjasik@jasik.xyz>
2022-12-20 15:10:33 -0600

    Add incomplete interface test
    I'm unsure what the correct output is here, but resolving quickly is one requirement
    Does golang have test timeouts?

--- internal/js_scanner/js_scanner_test.go
+++ internal/js_scanner/js_scanner_test.go
@@ -527,6 +527,11 @@ export const foo = 0
 */`,
 			want: `export const foo = 0`,
 		},
+		{
+			name:   "incomplete interface",
+			source: "import { a } from \"a\";\nexport interface ",
+			want:   "import { a } from \"a\";\nexport interface ",
+		},
 	}
 }
 
