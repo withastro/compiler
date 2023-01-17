193064ff7537baad2f7d5e6c8e23f71d8f5377d8
Nate Moore <natemoo-re@users.noreply.github.com>
2023-01-17 09:01:53 -0600

    test(#486): add regression test (#703)
    * test(#486): add regression test
    
    * Update props-interface.ts
    
    Co-authored-by: Nate Moore <nate@astro.build>

---
+++ packages/compiler/test/basic/props-interface.ts
@@ -0,0 +1,27 @@
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import { transform } from '@astrojs/compiler';
+
+const FIXTURE = `---
+// eslint-disable-next-line no-undef
+export interface Props extends astroHTML.JSX.HTMLAttributes {}
+
+const props = { ...Astro.props } as Props;
+---
+
+<body class:list={props['class:list']}>
+  <slot></slot>
+</body>`;
+
+let result;
+test.before(async () => {
+  result = await transform(FIXTURE);
+});
+
+test('retains newlines around comment', () => {
+  assert.ok(result.code, 'Expected to compile');
+  assert.match(result.code, /\/\/ eslint-disable-next-line no-undef\n/g);
+  assert.equal(result.diagnostics.length, 0, 'Expected no diagnostics');
+});
+
+test.run();
