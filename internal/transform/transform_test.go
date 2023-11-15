c4d598af69e32a6afd957c13599cc3aceda0657b
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-11-15 13:40:18 +0000

    test: add test

--- internal/transform/transform_test.go
+++ internal/transform/transform_test.go
@@ -252,6 +252,11 @@ func TestFullTransform(t *testing.T) {
 			want: `<Component><h1>Hello world</h1></Component>`,
 		},
 		{
+			name:   "Component before html",
+			source: `<Navigation /><html><body><h1>Astro</h1></body></html>`,
+			want:   `<Navigation></Navigation><h1>Astro</h1>`,
+		},
+		{
 			name:   "respects explicitly authored elements",
 			source: `<html><Component /></html>`,
 			want:   `<html><Component></Component></html>`,
