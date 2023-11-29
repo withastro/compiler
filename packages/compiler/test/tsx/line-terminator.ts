88fa4c2e5a3ec8065b690bfdadbd01e4a1170cee
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-11-29 22:26:43 +0000

    test: add tsx test

--- packages/compiler/test/tsx/line-terminator.ts
+++ packages/compiler/test/tsx/line-terminator.ts
@@ -1,14 +1,17 @@
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
-import { convertToTSX } from '@astrojs/compiler'; 
-    
-test('handles non-standard line terminators', async () => {
-  const input = ` `;
+import { convertToTSX } from '@astrojs/compiler';
+
+test('handles non-standard line terminators', async () =>
+{
+  const inputs = [` `, `something something`, `something  `, `   `, ];
   let err = 0;
-  try {
-    await convertToTSX(input, { filename: 'index.astro', sourcemap: 'inline' });
-  } catch (e) {
-    err = 1;
+  for (const input of inputs){
+    try {
+      await convertToTSX(input, { filename: 'index.astro', sourcemap: 'inline' });
+    } catch (e) {
+      err = 1;
+    }
   }
   assert.equal(err, 0, 'did not error');
 });
