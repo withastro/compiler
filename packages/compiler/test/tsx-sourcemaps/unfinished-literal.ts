9b25f39d0f4c3b110425604797678c2dbdcd4f9d
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-12-22 16:44:23 +0000

    add test

--- packages/compiler/test/tsx-sourcemaps/unfinished-literal.ts
+++ packages/compiler/test/tsx-sourcemaps/unfinished-literal.ts
@@ -2,14 +2,26 @@ import { test } from 'uvu';
 import * as assert from 'uvu/assert';
 import { convertToTSX } from '@astrojs/compiler';
 
-const input = `<div class=\`></div>
-`;
-
 test('does not panic on unfinished template literal attribute', async () => {
+  const input = `<div class=\`></div>
+  `;
   let error = 0;
   try {
     const output = await convertToTSX(input, { filename: 'index.astro', sourcemap: 'inline' });
     assert.match(output.code, `class={\`\`}`);
+  } catch (e) {
+    error = 1;
+  }
+
+  assert.equal(error, 0, `compiler should not have panicked`);
+});
+
+test('does not panic on unfinished quoted attribute', async () => {
+  const input = `<main id="gotcha />`;
+  let error = 0;
+  try {
+    const output = await convertToTSX(input, { filename: 'index.astro', sourcemap: 'inline' });
+    assert.match(output.code, `id=""`);
   } catch (e) {
     error = 1;
   }
