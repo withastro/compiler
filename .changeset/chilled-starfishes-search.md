b52f7d117356ed4a8f3fe970ceb3f4fe30278d4c
Happydev <81974850+MoustaphaDev@users.noreply.github.com>
2024-01-04 18:38:33 +0000

    fix(tokenizer): handle unterminated quoted attributes gracefully (#923)
    * add test
    
    * fix
    
    * chore: changeset
    
    * test: add single quoted attribute test
    
    * add (seemingly incorrect?) test for feedback
    
    * apply feedback to tests
    
    * simple heuristic to rescue some chars off attrs
    
    * update TS tests to refect changes
    
    * simplify whitespace check
    
    * Update internal/token.go
    
    * Update internal/token.go
    
    ---------
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>

---
+++ .changeset/chilled-starfishes-search.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/compiler': patch
+---
+
+Fixes an issue where unterminated quoted attributes caused the compiler to crash
--- internal/printer/printer_test.go
+++ internal/printer/printer_test.go
@@ -3342,6 +3342,21 @@ const c = '\''
 			source: `<html><body><h1>Hello world!</h1><style></style></body></html>`,
 			want:   []ASTNode{{Type: "element", Name: "html", Children: []ASTNode{{Type: "element", Name: "body", Children: []ASTNode{{Type: "element", Name: "h1", Children: []ASTNode{{Type: "text", Value: "Hello world!"}}}, {Type: "element", Name: "style"}}}}}},
 		},
+		{
+			name:   "element with unterminated double quote attribute",
+			source: `<main id="gotcha />`,
+			want:   []ASTNode{{Type: "element", Name: "main", Attributes: []ASTNode{{Type: "attribute", Kind: "quoted", Name: "id", Value: "gotcha", Raw: "\"gotcha"}}}},
+		},
+		{
+			name:   "element with unterminated single quote attribute",
+			source: `<main id='gotcha />`,
+			want:   []ASTNode{{Type: "element", Name: "main", Attributes: []ASTNode{{Type: "attribute", Kind: "quoted", Name: "id", Value: "gotcha", Raw: "'gotcha"}}}},
+		},
+		{
+			name:   "element with unterminated template literal attribute",
+			source: `<main id=` + BACKTICK + `gotcha />`,
+			want:   []ASTNode{{Type: "element", Name: "main", Attributes: []ASTNode{{Type: "attribute", Kind: "template-literal", Name: "id", Value: "gotcha", Raw: "`gotcha"}}}},
+		},
 	}
 
 	for _, tt := range tests {
