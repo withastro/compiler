15abc3ebfb72f33156a56c2367134c43db34499c
Nate Moore <nate@astro.build>
2024-01-12 12:26:43 -0600

    fix(#936): safer handling for GetLineAndColumnForLocation

---
+++ packages/compiler/test/parse/multibyte-characters.ts
@@ -0,0 +1,21 @@
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import { parse } from '@astrojs/compiler';
+
+const FIXTURE = `{foo}，`;
+
+test('does not crash', async () => {
+  const result = await parse(FIXTURE);
+  assert.ok(result.ast, 'does not crash');
+});
+
+test('properly maps the position', async () => {
+  const {
+    ast: { children },
+  } = await parse(FIXTURE);
+  const text = children[1];
+  assert.equal(text.position.start.offset, 5, 'properly maps the text start position');
+  assert.equal(text.position.end.offset, 8, 'properly maps the text end position');
+});
+
+test.run();
