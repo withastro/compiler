7d9e7d41447f0639cdc01c41a83f298e7f6a04d9
Arsh <69170106+lilnasy@users.noreply.github.com>
2024-02-16 22:34:57 +0530

    Apply suggestions from code review
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>

--- internal/transform/transform.go
+++ internal/transform/transform.go
@@ -409,7 +409,7 @@ func HintAboutImplicitInlineDirective(n *astro.Node, h *handler.Handler) {
 	if n.Type == astro.ElementNode && n.DataAtom == a.Script && len(n.Attr) > 0 && !HasInlineDirective(n) {
 		h.AppendHint(&loc.ErrorWithRange{
 			Code:  loc.HINT,
-			Text:  "Astro processes your script tags to allow using TypeScript and NPM packages, and to optimize browser performance.\n\nAttributes cannot be used on Astro-processed script tags. Therefore, this script tag will be treated as if it has the `is:inline` directive, opting it out of the processing steps and its features.\n\nFor clarity, you might want to add the `is:inline` directive explicitly.\n\nSee docs for more details: https://docs.astro.build/en/guides/client-side-scripts/#script-processing.",
+			Text:  "Astro processes your script tags to allow using TypeScript and npm packages, and to optimize browser performance.\n\nAttributes cannot be used on Astro-processed script tags. Therefore, this script tag will be treated as if it has the `is:inline` directive, opting it out of the processing steps and its features.\n\nFor clarity, you might want to add the `is:inline` directive explicitly.\n\nSee docs for more details: https://docs.astro.build/en/guides/client-side-scripts/#script-processing.",
 			Range: loc.Range{Loc: n.Attr[0].KeyLoc, Len: len(n.Attr[0].Key)},
 		})
 
