b3d401844175621657fc5780ad37704da5ed3a42
lilnasy <69170106+lilnasy@users.noreply.github.com>
2024-02-20 17:07:15 +0000

    shorten the lint message

--- internal/transform/transform.go
+++ internal/transform/transform.go
@@ -412,7 +412,7 @@ func HintAboutImplicitInlineDirective(n *astro.Node, h *handler.Handler) {
 		}
 		h.AppendHint(&loc.ErrorWithRange{
 			Code:  loc.HINT,
-			Text: "This script contains an attribute (ex: `type="module"`, `type="text/partytown"`, `async`) or a directive (`define:vars`) and will not be processed and bundled for optimized browser performance.\n\nThis script will be treated as if it has the `is:inline` directive and therefore features that require processing (e.g. using TypeScript or npm packages in the script) are unavailable.\n\nSee docs for more details: https://docs.astro.build/en/guides/client-side-scripts/#script-processing.",
+			Text:  "This script will be treated as if it has the `is:inline` directive, because it contains an attribute. Therefore features that require processing (e.g. using TypeScript or npm packages in the script) are unavailable.\n\nSee docs for more details: https://docs.astro.build/en/guides/client-side-scripts/#script-processing.",
 			Range: loc.Range{Loc: n.Attr[0].KeyLoc, Len: len(n.Attr[0].Key)},
 		})
 	}
--- packages/compiler/test/scripts/isinline-hint.ts
+++ packages/compiler/test/scripts/isinline-hint.ts
@@ -5,7 +5,7 @@ import { transform } from '@astrojs/compiler';
 test('reports a hint for adding attributes to a script tag without is:inline', async () => {
   const result = await transform(`<script type="module"></script>`);
   assert.equal(result.diagnostics[0].severity, 4);
-  assert.match(result.diagnostics[0].text, /Astro processes your script tags to allow using TypeScript and npm packages/);
+  assert.match(result.diagnostics[0].text, /\#script-processing/);
 });
 
 test('does not report a diagnostic for the src attribute', async () => {
