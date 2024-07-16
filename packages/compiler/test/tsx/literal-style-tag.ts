9549bb7bb71469eb6ff2427b7e149344e410daa0
Happydev <81974850+MoustaphaDev@users.noreply.github.com>
2024-07-16 13:42:36 +0000

    Prevent `<style>` from getting moved in `<html>` (#974)
    * chore: `only` helper for json test
    
    * fix: remove divergence from html spec
    
    * test: add test for #712
    
    * `addLoc` before popping the stack of oe
    
    * test: add test
    
    * literal parsing after `body` and `html`
    
    * test: add test for style tag after body
    
    * test: add tsx tests
    
    * test: add missing semicolon in test
    
    * test: add more tests from duplicates
    
    * nit: formatting
    
    * chore: changeset
    
    * test: add AST tests
    
    ---------
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>
    Co-authored-by: Erika <3019731+Princesseuh@users.noreply.github.com>

---
+++ packages/compiler/test/tsx/literal-style-tag.ts
@@ -0,0 +1,64 @@
+import { convertToTSX } from '@astrojs/compiler';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import { TSXPrefix } from '../utils.js';
+
+test('preserve style tag position I', async () => {
+	const input = `<html><body><h1>Hello world!</h1></body></html>
+<style></style>`;
+	const output = `${TSXPrefix}<Fragment>
+<html><body><h1>Hello world!</h1></body></html>
+<style></style>
+</Fragment>
+export default function __AstroComponent_(_props: Record<string, any>): any {}\n`;
+	const { code } = await convertToTSX(input, { sourcemap: 'external' });
+	assert.snapshot(code, output, 'expected code to match snapshot');
+});
+
+test('preserve style tag position II', async () => {
+	const input = `<html></html>
+<style></style>`;
+	const output = `${TSXPrefix}<Fragment>
+<html></html>
+<style></style>
+</Fragment>
+export default function __AstroComponent_(_props: Record<string, any>): any {}\n`;
+	const { code } = await convertToTSX(input, { sourcemap: 'external' });
+	assert.snapshot(code, output, 'expected code to match snapshot');
+});
+
+test('preserve style tag position III', async () => {
+	const input = `<html lang="en"><head><BaseHead /></head></html>
+<style>@use "../styles/global.scss";</style>`;
+	const output = `${TSXPrefix}<Fragment>
+<html lang="en"><head><BaseHead /></head></html>
+<style>{\`@use "../styles/global.scss";\`}</style>
+</Fragment>
+export default function __AstroComponent_(_props: Record<string, any>): any {}\n`;
+	const { code } = await convertToTSX(input, { sourcemap: 'external' });
+	assert.snapshot(code, output, 'expected code to match snapshot');
+});
+
+test('preserve style tag position IV', async () => {
+	const input = `<html lang="en"><head><BaseHead /></head><body><Header /></body></html>
+<style>@use "../styles/global.scss";</style>`;
+	const output = `${TSXPrefix}<Fragment>
+<html lang="en"><head><BaseHead /></head><body><Header /></body></html>
+<style>{\`@use "../styles/global.scss";\`}</style>
+</Fragment>
+export default function __AstroComponent_(_props: Record<string, any>): any {}\n`;
+	const { code } = await convertToTSX(input, { sourcemap: 'external' });
+	assert.snapshot(code, output, 'expected code to match snapshot');
+});
+
+test('preserve style tag position V', async () => {
+	const input = `<html lang="en"><head><BaseHead /></head><body><Header /></body><style>@use "../styles/global.scss";</style></html>`;
+	const output = `${TSXPrefix}<Fragment>
+<html lang="en"><head><BaseHead /></head><body><Header /></body><style>{\`@use "../styles/global.scss";\`}</style></html>
+</Fragment>
+export default function __AstroComponent_(_props: Record<string, any>): any {}\n`;
+	const { code } = await convertToTSX(input, { sourcemap: 'external' });
+	assert.snapshot(code, output, 'expected code to match snapshot');
+});
+
+test.run();
