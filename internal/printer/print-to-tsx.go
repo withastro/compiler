f86783ac6a0e64a98ce022ec3bfc6ff0ddcaf3cb
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-07-17 17:30:08 +0200

    fix: escape scripts with unknown types

--- internal/printer/print-to-tsx.go
+++ internal/printer/print-to-tsx.go
@@ -244,6 +244,7 @@ const (
 	Text
 	ScriptText
 	JsonScriptText
+	UnknownScriptText
 	StyleText
 )
 
@@ -254,9 +255,13 @@ func getTextType(n *astro.Node) TextType {
 			return ScriptText
 		}
 
-		if attr != nil && ScriptJSONMimeTypes[strings.ToLower(attr.Val)] {
+		// There's no difference between JSON and unknown script types in the result JSX at this time
+		// however, we might want to add some special handling in the future, so we keep them separate
+		if ScriptJSONMimeTypes[strings.ToLower(attr.Val)] {
 			return JsonScriptText
 		}
+
+		return UnknownScriptText
 	}
 	if style := n.Closest(isStyle); style != nil {
 		return StyleText
@@ -395,9 +400,9 @@ declare const Astro: Readonly<import('astro').AstroGlobal<%s, typeof %s`, propsI
 				p.print("}}\n")
 			}
 			p.addSourceMapping(loc.Loc{Start: n.Loc[0].Start + len(n.Data)})
-		} else if textType == StyleText || textType == JsonScriptText || textType == RawText {
+		} else if textType == StyleText || textType == JsonScriptText || textType == RawText || textType == UnknownScriptText {
 			p.addNilSourceMapping()
-			if (textType == StyleText && o.IncludeStyles) || textType == JsonScriptText || textType == RawText {
+			if (textType == StyleText && o.IncludeStyles) || ((textType == JsonScriptText || textType == RawText || textType == UnknownScriptText) && o.IncludeScripts) {
 				p.print("{`")
 				p.printTextWithSourcemap(escapeText(n.Data), n.Loc[0])
 				p.addNilSourceMapping()
