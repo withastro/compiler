3ceda668bd58becefd96b221c3897aa3f4c00d27
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-07-17 23:24:11 +0200

    fix(tsx): Transform top level returns into throws

--- internal/printer/print-to-tsx.go
+++ internal/printer/print-to-tsx.go
@@ -2,6 +2,7 @@ package printer
 
 import (
 	"fmt"
+	"slices"
 	"strings"
 	"unicode"
 
@@ -361,11 +362,31 @@ declare const Astro: Readonly<import('astro').AstroGlobal<%s, typeof %s`, propsI
 		p.addSourceMapping(loc.Loc{Start: 0})
 		frontmatterStart := len(p.output)
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			topLevelReturn := js_scanner.FindTopLevelReturns([]byte(c.Data))
 			if c.Type == TextNode {
 				if len(c.Loc) > 0 {
 					p.addSourceMapping(c.Loc[0])
 				}
-				p.printTextWithSourcemap(c.Data, c.Loc[0])
+				// Remplace all the top level returns with a `throw`
+				if topLevelReturn != nil {
+					// Loop over the characters and replace the top level returns with a `throw`
+					newString := ""
+
+					i := 0
+					for i < len(c.Data) {
+						if slices.Contains(topLevelReturn, i) {
+							newString += "throw "
+							i += 6
+						} else {
+							newString += string(c.Data[i])
+							i++
+						}
+					}
+
+					p.printTextWithSourcemap(newString, c.Loc[0])
+				} else {
+					p.printTextWithSourcemap(c.Data, c.Loc[0])
+				}
 			} else {
 				renderTsx(p, c, o)
 			}
---
+++ packages/compiler/test/tsx/top-level-returns.ts
@@ -0,0 +1,35 @@
+import { convertToTSX } from '@astrojs/compiler';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import { TSXPrefix } from '../utils.js';
+
+test('transforms top-level returns to throw statements', async () => {
+	const input = `---
+if (something) {
+	return Astro.redirect();
+}
+
+function thatDoesSomething() {
+	return "Hey";
+}
+
+class Component {
+	render() {
+		return "wow"!
+	}
+}
+---`;
+	const output = `${TSXPrefix}
+if (something) {
+	throw  Astro.redirect();
+}
+
+function thatDoesSomething() {
+	return "Hey";
+}
+`;
+	const { code } = await convertToTSX(input, { sourcemap: 'external' });
+	assert.snapshot(code, output, 'expected code to match snapshot');
+});
+
+test.run();
