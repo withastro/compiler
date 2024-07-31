21b7b9531d236bbb74bf4af58c87a5bb1a40c6fd
Erika <3019731+Princesseuh@users.noreply.github.com>
2024-07-31 17:53:15 +0200

    revert: top level returns transformations (#1036)

---
+++ .changeset/brown-terms-sleep.md
@@ -0,0 +1,5 @@
+---
+"@astrojs/compiler": patch
+---
+
+Revert the transformation of top-level returns into throws in TSX as it was buggy in numerous situations
--- internal/printer/print-to-tsx.go
+++ internal/printer/print-to-tsx.go
@@ -2,7 +2,6 @@ package printer
 
 import (
 	"fmt"
-	"slices"
 	"strings"
 	"unicode"
 
@@ -404,31 +403,11 @@ declare const Astro: Readonly<import('astro').AstroGlobal<%s, typeof %s`, propsI
 		p.addSourceMapping(loc.Loc{Start: 0})
 		frontmatterStart := len(p.output)
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			topLevelReturn := js_scanner.FindTopLevelReturns([]byte(c.Data))
 			if c.Type == TextNode {
 				if len(c.Loc) > 0 {
 					p.addSourceMapping(c.Loc[0])
 				}
-				// Remplace all the top level returns with a `throw`
-				if len(topLevelReturn) > 0 {
-					// Loop over the characters and replace the top level returns with a `throw`
-					newString := []byte{}
-
-					i := 0
-					for i < len(c.Data) {
-						if slices.Contains(topLevelReturn, i) {
-							newString = append(newString, []byte("throw ")...)
-							i += len("return")
-						} else {
-							newString = append(newString, c.Data[i])
-							i++
-						}
-					}
-
-					p.printTextWithSourcemap(string(newString), c.Loc[0])
-				} else {
-					p.printTextWithSourcemap(c.Data, c.Loc[0])
-				}
+				p.printTextWithSourcemap(c.Data, c.Loc[0])
 			} else {
 				renderTsx(p, c, o)
 			}
