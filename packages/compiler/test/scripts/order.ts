fc1bef9dd611689b3c09d22da5f96348288fec3f
Reuben Tier <otterlord.dev@gmail.com>
2025-01-03 23:55:50 +0000

    Fix js lint

--- packages/compiler/test/scripts/order.ts
+++ packages/compiler/test/scripts/order.ts
@@ -7,11 +7,11 @@ test('outputs scripts in expected order', async () => {
     <script>console.log(1)</script>
     <script>console.log(2)</script>`);
 
-	const scripts = result.scripts
+	const scripts = result.scripts;
 
 	// for typescript
-	if (scripts[0].type === 'external') throw new Error("Script is external")
-	if (scripts[1].type === 'external') throw new Error("Script is external")
+	if (scripts[0].type === 'external') throw new Error('Script is external');
+	if (scripts[1].type === 'external') throw new Error('Script is external');
 
 	assert.match(scripts[0].code, 'console.log(1)');
 	assert.match(scripts[1].code, 'console.log(2)');
