27b95d56aabec1f5931b05141eab56916b75e7b7
Liam Rooney <liam@roon.dev>
2025-04-23 23:06:40 -0700

    fix: allow options to contain html elements

--- internal/parser.go
+++ internal/parser.go
@@ -2358,6 +2358,8 @@ func inSelectIM(p *parser) bool {
 			p.resetInsertionMode()
 		case a.Template:
 			return inHeadIM(p)
+		default:
+			return inBodyIM(p)
 		}
 	case CommentToken:
 		p.addChild(&Node{
