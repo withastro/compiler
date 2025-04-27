6ff989e5c0e74b00d47f60ef9e77544f9e6cb5f2
Moustapha HappyDev <moustapha.happydev@gmail.com>
2025-04-27 02:33:43 +0000

    test(transform): reflect trivia now being included with hoisted symbols

--- packages/compiler/test/basic/export.ts
+++ packages/compiler/test/basic/export.ts
@@ -32,7 +32,11 @@ export type FuncType = (x: number) => number;
 	const result = await transform(FIXTURE);
 	assert.match(
 		result.code,
-		'export type NumberType = number;\nexport type FuncType = (x: number) => number',
+		`// this is fine
+export type NumberType = number;
+// astro hangs because of this typedef.
+// comment it out and astro will work fine.
+export type FuncType = (x: number) => number`,
 		'Expected output to contain full Props export'
 	);
 });
